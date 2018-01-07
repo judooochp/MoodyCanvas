3bbe400ff6dd6d3211750c25634d2e34196d94f6
Jake <jakeegill@gmail.com>
2018-01-07 00:24:09 -0600

    Delete duplicate files in MoodyCanvas folder. Edit Readme

--- MoodyCanvas/MoodyCanvas/Moody/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer, Plate, Verif, Profile, Meas
-
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Plate)
-admin.site.register(Verif)
-admin.site.register(Profile)
-admin.site.register(Meas)
